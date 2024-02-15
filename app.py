386daf4b200715e768750cbb30bbcf84d27b6d39
arbak <l.arbaczewski@gmail.com>
2024-02-15 16:47:42 +0100

    update settings

--- app.py
+++ app.py
@@ -16,61 +16,36 @@ logger.info("This message will be logged to application logs")
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     print('Request for index page received')
 
-    # Azure PostgreSQL connection parameters
-    host = os.environ.get('HOST')
-    dbname = os.environ.get('DATABASE')
-    user = os.environ.get('USER_IDENTITY')
-    sslmode = 'require'
+    # For user-assigned identity.
+    managed_identity_client_id = os.getenv('AZURE_POSTGRESQL_CLIENTID')
+    cred = ManagedIdentityCredential(client_id=managed_identity_client_id)
 
-    # Acquire AAD token
+    logger.info("This Client new Identificatie ID: {}".format(cred))
 
-    # Retrieving the client ID from environment variable
-    client_id = os.environ.get('CLIENT_ID')
-    cred = ManagedIdentityCredential(client_id=client_id)
-    # Logging the client ID
-    logger.info("This Client new Identificatie ID: {}".format(client_id))
+    # Acquire the access token
+    accessToken = cred.get_token('https://ossrdbms-aad.database.windows.net/.default')
 
-    token = cred.get_token('https://ossrdbms-aad.database.windows.net/.default')
-    access_token = token.token
+    # Combine the token with the connection string from the environment variables added by Service Connector to
+    # establish the connection.
+    conn_string = os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING')
+    connection = psycopg2.connect(conn_string + ' password=' + accessToken.token)
+    print("Connection established")
+    logger.info("Connection established")
+    connection.close()
 
-    logger.info("This message new about token: {}".format(str(access_token[0:2])))
-
-    conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, access_token,
-                                                                                 sslmode)
-    conn = psycopg2.connect(conn_string)
-    logger.info("User name new to test: " + user + ".")
-
-    try:
-        logger.info("Connection established")
-        cursor = conn.cursor()
-
-        # Example parameterized query
-        query = "SELECT * FROM public.bag_panden ORDER BY id ASC LIMIT 100"
-
-        # Execute query with parameters
-        cursor.execute(query)
-
-        # Fetch results
-        rows = cursor.fetchall()
-        for row in rows:
-            logger.info("User name to test: " + row + ".")
-
-        # Close cursor and connection
-        cursor.close()
-        conn.close()
-
-    except psycopg2.Error as e:
-        print(f"Error: {e}")
     return render_template('index.html')
+
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 
 @app.route('/hello', methods=['POST'])
 def hello():
@@ -82,6 +57,7 @@ def hello():
     else:
         print('Request for hello page received with no name or blank name -- redirecting')
         return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run()
