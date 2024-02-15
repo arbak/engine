7527f08172a7fb06c94085a67a944a134736dd4f
arbak <l.arbaczewski@gmail.com>
2024-02-15 09:57:23 +0100

    update

--- app.py
+++ app.py
@@ -1,25 +1,63 @@
 import os
-
+import logging
+import psycopg2
+from dotenv import load_dotenv
+from azure.identity import ManagedIdentityCredential
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
-import logging
+
+# Load environment variables from the .env file
+load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 logger.info("This message will be logged to application logs")
-from azure.identity import ManagedIdentityCredential
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   cred = ManagedIdentityCredential(client_id="6bbe6b4c-aee5-43f6-8845-1646ddb3d95b")
-   token = cred.get_token('https://ossrdbms-aad.database.windows.net/.default')
-   content = token.token
-   logger.info("This message will be logged to application logs: " + str(content[0:2]) + ".")
-   return render_template('index.html')
+    print('Request for index page received')
+
+    # Azure PostgreSQL connection parameters
+    host = os.environ.get('HOST')
+    dbname = os.environ.get('DATABASE')
+    user = os.environ.get('USERNAME')
+    sslmode = 'require'
+
+    # Acquire AAD token
+    cred = ManagedIdentityCredential(client_id=os.environ.get('CLIENT_ID'))
+    token = cred.get_token('https://ossrdbms-aad.database.windows.net/.default')
+    access_token = token.token
+
+    conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, access_token,
+                                                                                 sslmode)
+    conn = psycopg2.connect(conn_string)
+    logger.info("User name to test: " + user + ".")
+
+    try:
+        logger.info("Connection established")
+        cursor = conn.cursor()
+
+        # Example parameterized query
+        query = "SELECT * FROM public.bag_panden ORDER BY id ASC LIMIT 100"
+
+        # Execute query with parameters
+        cursor.execute(query)
+
+        # Fetch results
+        rows = cursor.fetchall()
+        for row in rows:
+            logger.info("User name to test: " + row + ".")
+
+        # Close cursor and connection
+        cursor.close()
+        conn.close()
+
+    except psycopg2.Error as e:
+        print(f"Error: {e}")
+    return render_template('index.html')
 
 @app.route('/favicon.ico')
 def favicon():
@@ -28,15 +66,14 @@ def favicon():
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
+    name = request.form.get('name')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
+    if name:
+        print('Request for hello page received with name=%s' % name)
+        return render_template('hello.html', name=name)
+    else:
+        print('Request for hello page received with no name or blank name -- redirecting')
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
